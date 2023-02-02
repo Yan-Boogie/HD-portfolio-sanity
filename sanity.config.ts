da6c727b843931d58be5070470da3c4c7409fb0d
Ian Lai <ian.lai@houzz.com>
2023-02-02 14:09:40 +0800

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'hd-design-portfolio-sanity',
+
+  projectId: 'av5zugn5',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
