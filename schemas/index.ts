d25ed3c597d01ad92cad03a0e742bf0cdaa36443
Ian Lai <ian.lai@houzz.com>
2023-02-09 23:10:48 +0800

    delete: remove example files

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,22 +1,14 @@
 import blockContent from './blockContent'
-import crewMember from './crewMember'
-import castMember from './castMember'
-import movie from './movie'
-import person from './person'
-import screening from './screening'
-import plotSummary from './plotSummary'
-import plotSummaries from './plotSummaries'
+import showreel from './showreel'
+import work from './work'
+import video from './video'
 
 export const schemaTypes = [
   // Document types
-  movie,
-  person,
-  screening,
+  showreel,
+  work,
 
   // Other types
   blockContent,
-  plotSummary,
-  plotSummaries,
-  castMember,
-  crewMember,
+  video,
 ]
